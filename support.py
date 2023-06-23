3ca488ed362fa98755696c2f605a81343b468dc0
haider-06418 <ma06418@st.habib.edu.pk>
2023-06-23 18:33:06 +0500

    address type classification

--- support.py
+++ support.py
@@ -1,59 +1,34 @@
-# import pandas as pd
-# import numpy as np
-# from ez_address_parser import AddressParser
-# import usaddress
-
-abbreviations = {
-    'Apt': 'Apartment',
-    'Bldg.': 'Building',
-    'Cantt.': 'Cantonment',
-    'Col.': 'Colony',
-    'Ext.': 'Extension',
-    'Hno.': 'House Number',
-    'Mkt.': 'Market',
-    'Ngr.': 'Nagar',
-    'P.O.': 'Post Office',
-    'P.O.B': 'Post Office Box',
-    'Pl.': 'Place',
-    'Sec.': 'Sector',
-    'Twn.': 'Town',
-    'Vlg.': 'Village',
-    'Dist.': 'District',
-    'Rd.': 'Road',
-    'Jct.': 'Junction',
-    'Ln.': 'Lane',
-    'Masjid': 'Mosque',
-    'Fl.': 'Floor',
-    'Blk.': 'Block',
-    'Sch.': 'School',
-    'Univ.': 'University',
-    'Corp.': 'Corporation',
-    'Ctr.': 'Center',
-    'Plz.': 'Plaza',
-    'Ph.': 'Phase',
-    'G.P.O.': 'General Post Office',
-    'Bzr.': 'Bazaar',
-    'Est.': 'Estate',
-    'St.': 'Street',
-    'Ave': 'Avenue',
-    'Blvd': 'Boulevard',
-    'Pkwy': 'Parkway',
-    'Hwy': 'Highway',
-    'Res.': 'Residential',
-    # Add more mappings as needed
-    }
+import data_preprocessor
 
 sample1 = 'House # C38, Block 8, Gulshan-e-Iqbal, Karachi'
 sample2 = 'House No. 123, Street 5, Phase 7, DHA'
 sample3 = "House No. 123, St. 5, Phase 7, DHA"
 
-def standard_abbreviations_fix(address, abbreviation_mapping):
-    words = address.split()
-    standardized_words = [abbreviation_mapping.get(word, word) for word in words]
-    standardized_address = ' '.join(standardized_words)
-    return standardized_address
+abbreviations = data_preprocessor.load_json("abbreviations.json")
 
 
-standardized_address = standard_abbreviations_fix(sample3, abbreviations)
+def check_address_type(address):
+    house_keywords = ['house no', 'house number', 'house #', 'house']
+    apartment_keywords = ['flat no', 'flat number', 'flat #', 'flat', 'apartment', 'building']
 
-print(standardized_address)
+    house_found = any(keyword in address for keyword in house_keywords)
+    apartment_found = any(keyword in address for keyword in apartment_keywords)
+
+    if house_found and apartment_found:
+        return 'Both House and Apartment'
+    elif house_found:
+        return 'House'
+    elif apartment_found:
+        return 'Apartment'
+    else:
+        return 'Unknown'
+
+
+
+addy = data_preprocessor.lowercase_conversion(sample1)
+
+standardized_address = data_preprocessor.standard_abbreviations_fix(addy, abbreviations)
+
+addy_type = check_address_type(standardized_address)
+
+print(addy_type)
