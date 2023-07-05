ac4874e4ad005407b18a08aba4bb909c95ac609b
haider-06418 <ma06418@st.habib.edu.pk>
2023-07-05 17:47:24 +0500

    testing

--- support.py
+++ support.py
@@ -2,7 +2,7 @@ import data_preprocessor
 
 sample1 = 'House # C38, Block 8, Gulshan-e-Iqbal, Karachi'
 sample2 = 'House No. 123, Street 5, Phase 7, DHA, Karachi'
-sample3 = "House No. 123, St. 5, Phase 7, DHA, Karachi"
+sample3 = "House No. 123, St 5, Phase 7, DHA, Karachi"
 
 abbreviations = data_preprocessor.load_json("abbreviations.json")
 
@@ -16,16 +16,6 @@ abbreviations = data_preprocessor.load_json("abbreviations.json")
 #     elif address_type == 'unknown':
 #         pass
 
-lowered_address = data_preprocessor.lowercase_conversion(sample3)
-standardized_address = data_preprocessor.remove_punctuation(lowered_address, True)
-standardized_address = data_preprocessor.standard_abbreviations_fix(standardized_address, abbreviations)
-standardized_address = data_preprocessor.remove_extra_spaces(standardized_address, True)
+sample4 = 'Apartment/Suite# 404 4th Floor, Building All Blocks, Fatima Tower, Jamaludin Afghani Rd, Block 3, PECHS, Karachi'
 
-address_type = data_preprocessor.check_address_type(standardized_address)
-tokenized_address = data_preprocessor.standard_tokenization(standardized_address)
-
-print(address_type)
-print(tokenized_address)
-
-print(standardized_address)
 
