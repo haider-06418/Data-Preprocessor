414c360c51cbe2cff7bad9875072a22f16a36fec
haider-06418 <ma06418@st.habib.edu.pk>
2023-07-11 13:47:24 +0500

    probabilistic identifiers for missing fields

--- support.py
+++ support.py
@@ -39,9 +39,13 @@ samples = [' House # F-37 Off, Khayaban-e-Iqbal, Block 8, Clifton, Karachi ',
 
 sample6 = ' Apartment/Suite# 409 4th fl, Building All Blocks, Sahil Promenade, Khayaban e Sadi, Block 3, Clifton, Karachi '
 
-remaining_address = [' Building All Blocks', ' Sahil Promenade', ' Khayaban e Sadi']
+remaining_address = [' Building All Blocks', ' Sahil Promenade', ' Khayaban e Sadi', ' Block 3']
+# remaining_address = [' Building All Blocks', ' Sahil Promenade', ' Khayaban e Sadi']
+# remaining_address = [' Khayaban e Sadi', ' Block 3']
+# remaining_address = [' Building All Blocks', ' Sahil Promenade']
 
 data = {'Ticket #': [], 'Type': [], 'House #': [], 'Apartment #': [], 'Building #': [], 'Building Name': [], 'Street Number/Name': [], 'Area & Sub Area': [], 'Neighbourhood': [], 'City': []}
+
 
 def probabilistic_identifiers(sample_address):
     tokenized_address = data_preprocessor.standard_tokenization(sample_address) 
@@ -50,23 +54,78 @@ def probabilistic_identifiers(sample_address):
     # to_place = len(remaining_address)
 
     index_p_scores = []
+    count = 0
 
     for item in remaining_address:
         true_index_in_original = reference_tokenized_address.index(item)+1
         index_percentage = (true_index_in_original/len(reference_tokenized_address))*100
-        index_p_scores.append(index_percentage)
+        index_p_scores.append((count, index_percentage))
+        count += 1
 
-    for score in index_p_scores:
-        if score < 50:
-            if score == sorted(index_p_scores)[0]:
-                return 1
+    # for score in index_p_scores:
+    #     if score < 50:
+    #         if score == sorted(index_p_scores)[0]:
+    #             return 1
+
+    potienal_area = [(index, score) for index, score in index_p_scores if score > 50]
+
+    remaining_fields = [(index, score) for index, score in index_p_scores if score <= 50]
+
+    if len(remaining_fields) >= 2:
+
+        max_score_index = max(remaining_fields, key=lambda x: x[1])[0]
+        potienal_building_name_tuple = remaining_fields.pop(max_score_index)
+        potienal_building_name = list()
+        potienal_building_name.append(potienal_building_name_tuple)
+
+        potienal_building_number = list(remaining_fields)
+
+        # if len(remaining_fields) == 1:
+        #     potienal_building_number = list(remaining_fields.pop())
+        # else:
+        #     potienal_building_number = list(remaining_fields)
+    
+    elif len(remaining_fields) == 1:
+        potienal_building_name = list(remaining_fields)
+
+    else:
+        potienal_building_name, potienal_building_number = [], []
 
 
-    return index_p_scores
+    # print(potienal_area)
+    # print(potienal_building_name)
+    # print(potienal_building_number)
+
+    # if len(remaining_fields) < 2:
+
+    # return index_p_scores
+
+    areas_indexes = [ip_tuple[0] for ip_tuple in potienal_area]
+    building_name_indexes = [ip_tuple[0] for ip_tuple in potienal_building_name]
+    building_number_indexes = [ip_tuple[0] for ip_tuple in potienal_building_number]
+
+    # return potienal_area, potienal_building_name, potienal_building_number
+    return areas_indexes, building_name_indexes, building_number_indexes
 
 
-print(probabilistic_identifiers(sample6))
+# print(probabilistic_identifiers(sample6))
+
+ai, bni, bnui = probabilistic_identifiers(sample6)
+
+print('Area Index: ', ai)
+print('Building Name Index:', bni)
+print('Building Number Index: ', bnui)
 
 
-# if len(tokenized_address) == 1:
-#     pass
+
+
+
+# lst = [(0, 28.57142857142857), (1, 42.857142857142854), (2, 57.14285714285714)]
+
+# lowest_score_index = min(lst, key=lambda x: x[1])[0]
+
+# print(lowest_score_index) 
+
+# max_score_index = max(lst, key=lambda x: x[1])[0]
+
+# print(max_score_index) 
