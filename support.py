761a968df08a9802b9ba6b1f42e7ca0a6c968707
haider-06418 <ma06418@st.habib.edu.pk>
2023-07-18 14:15:11 +0500

    test data for comma placement issue

--- support.py
+++ support.py
@@ -1,37 +1,39 @@
-import data_preprocessor
+# import data_preprocessor
 
 # abbreviations = data_preprocessor.load_json("abbreviations.json")
 
-sample1 = 'House # C38, Block 8, Gulshan-e-Iqbal, Karachi'
-sample2 = 'House No. 123, Street 5, Phase 7, DHA, Karachi'
-sample3 = "House No. 123, St 5, Phase 7, DHA, Karachi"
-sample4 = 'Apartment/Suite# 404 4th Floor, Building All Blocks, Fatima Tower, Jamaludin Afghani Rd, Block 3, PECHS, Karachi'
-
-samples = [' House # F-37 Off, Khayaban-e-Iqbal, Block 8, Clifton, Karachi ', 
-           ' House # 82-B ,1st Floor, 21st Street, Khayaban e badar, Phase 6, Defence, Karachi ', 
-           ' Apartment/Suite# 702 7th Floor, Building All Block, Jheel ParkView, Siraj Road, Block 2, PECHS, Karachi ', 
-           ' House # R-18, Street No 37, Block-1, Gulistan-e-Johar, Karachi ', 
-           ' Apartment/Suite# 409 4th fl, Building All Blocks, Sahil Promenade, Khayaban e Sadi, Block 3, Clifton, Karachi ', 
-           ' House # 6C 1st Floor Ismail Center, Alamgir Road, Bahadurabad, PECHS, Karachi ', 
-           ' Apartment/Suite# 204 2nd Floor, Building C, Farhan Classic, Pro Abdul Ghafoor Rd, Block 12, Gulistan-e-Johar, Karachi ', 
-           ' House # 16B/1 Main, National Highway, Phase 2, Defence, Karachi ', 
-           ' House # 187/2/B Off, Shahrah e Qaideen, Block 2, PECHS, Karachi ', 
-           ' House # R-11/1, Khayaban e Sadi, Phase 7, Defence, Karachi ']
-
-sample5 = ' House # 82-B ,1st Floor, 21st Street, Khayaban e badar, Phase 6, Defence, Karachi '
-sample6 = ' Apartment/Suite# 409 4th fl, Building All Blocks, Sahil Promenade, Khayaban e Sadi, Block 3, Clifton, Karachi '
-
-# remaining_address = [' Building All Blocks', ' Sahil Promenade', ' Khayaban e Sadi', ' Block 3']
-# remaining_address2 = [' Building All Blocks', ' Sahil Promenade', ' Khayaban e Sadi']
-# remaining_address3 = [' Khayaban e Sadi', ' Block 3']
-# remaining_address4 = [' Building All Blocks', ' Sahil Promenade']
-
-# sample7 = 'Apartment/Suite# Flat 204, Building All Blocks, Pardesi Green Land Apartments, 8th Street, near baitul mukarrram majid, behind hakim saeed park, Bath Island, Clifton, Karachi'
-# remaining_address = [' Building All Blocks', ' Pardesi Green Land Apartments', ' near baitul mukarrram majid', ' behind hakim saeed park', ' Bath Island']
-
-# sample8 = ' House # 82-B ,1st Floor, near baitul mukarrram majid, behind hakim saeed park, 21st Street, Khayaban e badar, Phase 6, Defence, Karachi '
-# remaining_address = [ ' near baitul mukarrram majid', ' behind hakim saeed park', ' Khayaban e badar', ' Phase 6']
-# remaining_address = [ ' near baitul mukarrram majid', ' behind hakim saeed park', ' Khayaban e badar']
-
 data = {'Ticket #': [], 'Type': [], 'House #': [], 'Apartment #': [], 'Building #': [], 'Building Name': [], 'Street Number/Name': [], 'Area & Sub Area': [], 'Neighbourhood': [], 'City': []}
 
+
+# incorrect tokenization: incorrect comma placement
+
+sample_addresses = [' House # 75/1 5th lane, Khayaban e badar, Phase 6, Defence, Karachi ',
+                    ' House # 64/2 23rd Street, Khayaban e Badban, Phase 5, Defence, Karachi ',
+                    ' House # 6C 1st Floor Ismail Center, Alamgir Road, Bahadurabad, PECHS, Karachi ', 
+                    ' House # 70/1 11th Street, Khayaban e badar, Phase 6, Defence, Karachi ',
+                    ' House # 3/37 Molimabad, Khalid Bin Walid Rd, Block 3, PECHS, Karachi ',
+                    ' House # 159 N  SWEET HOMES FLAT B 2, Kashmir Road, Block 3, PECHS, Karachi ',
+                    ' House # Flat # 204 Al-Faisal Appartment, M Rafi Shamsi Road, Block 6, PECHS, Karachi ',
+                    ' House # 1 C 1st Floor 29th Street, Tauheed Commercial, Phase 5, Defence, Karachi ',
+                    ' Apartment/Suite# B-508, Building Block B, Billys Paradise Phase 2, Professor Abdul Ghafoor Road, Block 18, Gulistan-e-Johar, Karachi ',
+                    ' House # D-3, Sawera Grand, Gulshan e Faisal, Bath Island, Clifton, Karachi ', # 102632106168
+                    ' House # 1B 2nd South Street, South Circular Av, Phase 2, Defence, Karachi ',
+                    ' House # B-330, near Taimuria Police, Allama Rasheed Turab, Block L, North Nazimabad, Karachi ',
+                    ' House # 5 Flat 901 9th Floor Saima Burj Al Baraka, Khaliq-uz-Zaman Road, Block 8, Clifton, Karachi ',
+                    ' House # Saima Burj Al Baraka Apartment Flat No 603 6th Flo, Khaliq-uz-Zaman Road, Block 8, Clifton, Karachi ',
+                    ' House # 2-C APPT No 301 Floor 3rd 6th Lane, Nishat Commercial, Phase 6, Defence, Karachi ',
+                    ' House # 4E/2/10 Flat 2 Islamia Center Main, Sir Shah Suleman RD, Nazimabad No 4, Nazimabad, Karachi',
+                    ' House # Flat no. 5 2nd Floor Rabia Apartment Block 7/8, Ghazi Salahuddin Rd, CP Berar CHS, PECHS, Karachi ',
+                    ' House # 165 Flat 203 Pardesi Heights Numaish, Britto Road, West - Catholic Colony, Garden, Karachi ',
+                    ' House # WS-3 Flat no 401 4th Floor (Amafhha Arcade) Off, Burhani Avenue Road, KCHS, PECHS, Karachi', 
+                    ' House # Safety Homes Flat No 510 5th Floor, Daud Pota Road, Civil Lines, Clifton, Karachi ']
+
+
+'''We are doing address normalization, and an esssential part of it is tokenization. We are tokenizing with respect to the commas placed.
+It is working fine for many but for a significant chunck of addresss it is not working as the commmas are in correctly placed in them, making 
+the tokenization process incorrect, such that when it splits from an incorrectly placed comma 2 or more fields are placed in the same field. 
+Below the list sample_addresses contains sample of such addresses in which the commas are incorrectly placed. Device and code in python the 
+means by which we can correctly place commas so it can we tokenized appropriatly. 
+The address fields which we are using are the keys of the data dictionary given below and the values will be the correct address field.
+data = {'Ticket #': [], 'Type': [], 'House #': [], 'Apartment #': [], 'Building #': [], 'Building Name': [], 'Street Number/Name': [], 'Area & Sub Area': [], 'Neighbourhood': [], 'City': []}
+Your task is to just place the commas right so we can tokenize it correctly. '''
