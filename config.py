c5a93dc9093dbd93e35075e6d747f0c2787325fd
haider-06418 <ma06418@st.habib.edu.pk>
2023-07-20 15:00:31 +0500

    update config with global variables

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-fname = 'data/khi_tickets_2022.csv' 
-fname_normalized = 'data/datarand_a4.csv'
+# global variables
+
+# fname = 'data/khi_tickets_2022.csv'
+fname = 'data/test_data_excelnormalized.csv' 
+
+fname_normalized = 'data/test_normalized.csv'
+
+fname_normalized_excel = 'data/test_normalized.xlsx'
