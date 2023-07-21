592e2d716c98b3b940268f6bc3ba1a8cb4a087c8
haider-06418 <ma06418@st.habib.edu.pk>
2023-07-21 16:44:21 +0500

    minor edits

--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 fname = 'data/khi_tickets_2022.csv'
 # fname = 'data/test_data_excelnormalized.csv' 
 
-fname_normalized = 'data/khi_tickets_2022_test1_normalized.csv'
+# fname_normalized = 'data/khi_tickets_2022_test1_normalized.csv'
 
-fname_normalized_excel = 'data/khi_tickets_2022_test1_normalized.xlsx'
+fname_normalized = 'data/test20percent/khi_tickets_2022_test5_normalized.csv' 
+
+fname_normalized_excel = 'data/test20percent/khi_tickets_2022_test5_normalized.xlsx'
