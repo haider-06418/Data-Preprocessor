08c1f8347ca0168b1800df516926e13abf3aaf8d
haider-06418 <ma06418@st.habib.edu.pk>
2024-02-12 16:07:12 +0500

    config.py update

--- config.py
+++ config.py
@@ -1,17 +1,37 @@
 # global variables
 
+
+
+''''source file path:'''
 # fname = 'data/khi_tickets_2022/khi_tickets_2022.csv'
 # fname = 'data/khi_tickets_2022/khi_tickets_2022_unique.csv'
-fname = 'Data/buildings/Buildings_Dataset.csv'
+# fname = 'Data/buildings/Buildings_Dataset.csv'
 
-fname_normalized = 'data/buildings/normalized_buildings_dataset.csv'
+# fname = 'Data/current/Karachi_3rd_Quarter_2023.csv'
+fname = 'Data/current/Annual - Lahore Street_Building Extraction.csv'
+
+
+
+'''normalized csv file path:'''
+# fname_normalized = 'data/buildings/normalized_buildings_dataset.csv'
 # fname_normalized = 'data/khi_tickets_2022/normalized_khi_tickets_2022_all.csv'
 # fname_normalized = 'data/khi_tickets_2022/normalized_khi_tickets_2022_run9.csv'
 
-fname_normalized_excel = 'data/buildings/normalized_buildings_dataset.xlsx'
+# fname_normalized = 'Data/current/Karachi_3rd_Quarter_2023_normalized.csv'
+fname_normalized = 'Data/current/Annual - Lahore Street_Building Extraction_normalized.csv'
+
+
+
+'''normalized excel file path:'''
+# fname_normalized_excel = 'data/buildings/normalized_buildings_dataset.xlsx'
 # fname_normalized_excel = 'data/khi_tickets_2022/normalized_khi_tickets_2022_all.xlsx'
 # fname_normalized_excel = 'data/khi_tickets_2022/normalized_khi_tickets_2022_run9.xlsx'
 
+# fname_normalized_excel = 'Data/current/Karachi_3rd_Quarter_2023_normalized_excel.xlsx'
+fname_normalized_excel = 'Data/current/Annual - Lahore Street_Building Extraction_normalized_excel.xlsx'
 
+
+
+'''columns to drop: '''
 # columns_to_drop = ['Title', 'Created', 'Close Time', 'Queue']
-columns_to_drop = ['Building Name']
+# columns_to_drop = ['Building Name']
